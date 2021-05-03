2e0f4fbb84076e4f77a9c4f48ca7a388e73c0b85
Sebastian Berendt <sb@peytz.dk>
2021-05-03 20:53:15 +0200

    make the build script to make some magic

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-TAG="${1:-test}"
 
-docker build ./ansible-base -t mrscherrycoke/ansible-base:$TAG
-docker push mrscherrycoke/ansible-base:$TAG
+for TAG in $(git rev-parse --abbrev-ref HEAD) latest ; do
 
-for IMAGE in ansible-playbook ansible; do
-
-  sed -i "s/:PLACEHOLDER /:$TAG /g" $IMAGE/Dockerfile
-  docker build ./$IMAGE -t mrscherrycoke/$IMAGE:$TAG
-  sed -i "s/:$TAG /:PLACEHOLDER /g" $IMAGE/Dockerfile
-  docker push mrscherrycoke/$IMAGE:$TAG
-
+  docker build ./ansible-base -t mrscherrycoke/ansible-base:$TAG
+  docker push mrscherrycoke/ansible-base:$TAG
+  
+  for IMAGE in ansible-playbook ansible ansible-vault; do
+  
+    sed -i "s/:PLACEHOLDER /:$TAG /g" $IMAGE/Dockerfile
+    docker build ./$IMAGE -t mrscherrycoke/$IMAGE:$TAG
+    sed -i "s/:$TAG /:PLACEHOLDER /g" $IMAGE/Dockerfile
+    docker push mrscherrycoke/$IMAGE:$TAG
+  
+  done
 done
