d81e23c9dc068460fd64ba2da9749d25725191c8
Sebastian Berendt <sebastian.berendt@gmail.com>
2021-12-13 21:38:45 +0100

    update the build script with version

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-for TAG in $(git rev-parse --abbrev-ref HEAD) latest ; do
+set -e
 
-  docker build ./ansible-base -t mrscherrycoke/ansible-base:$TAG
-  docker push mrscherrycoke/ansible-base:$TAG
-  
-  for IMAGE in ansible-playbook ansible ansible-vault; do
-  
-    sed -i "s/:PLACEHOLDER /:$TAG /g" $IMAGE/Dockerfile
-    docker build ./$IMAGE -t mrscherrycoke/$IMAGE:$TAG
-    sed -i "s/:$TAG /:PLACEHOLDER /g" $IMAGE/Dockerfile
-    docker push mrscherrycoke/$IMAGE:$TAG
-  
+for VERSION in $(ls -1 ./ansible-base/requirements/); do
+  cp ./ansible-base/requirements/$VERSION ./ansible-base/requirements.txt
+
+  for TAG in $VERSION-$(git rev-parse --short HEAD) $VERSION; do
+
+    docker build ./ansible-base -t mrscherrycoke/ansible-base:$TAG
+    docker push mrscherrycoke/ansible-base:$TAG
+
+    for IMAGE in ansible-playbook ansible ansible-vault; do
+
+      sed -i "s/:PLACEHOLDER /:$TAG /g" $IMAGE/Dockerfile
+      docker build ./$IMAGE -t mrscherrycoke/$IMAGE:$TAG
+      sed -i "s/:$TAG /:PLACEHOLDER /g" $IMAGE/Dockerfile
+      docker push mrscherrycoke/$IMAGE:$TAG
+
+    done
   done
 done
